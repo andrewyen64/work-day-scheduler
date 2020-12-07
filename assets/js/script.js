abca4dae0da75e19aab4fcda687aaa9a04d46085
Andrew <andrewyen64@gmail.com>
2020-12-06 18:13:35 -0800

    Updated/removed/clean js code and comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
     // Displays current day at the top
     $("#currentDay").append(moment().format('dddd, MMMM Do'));
-        
-    // Changes Timeblocks color
+    var presentHour = moment().format("kk");
+    
+    // Set each text area to a variable
     var textBlock9 = $(".textarea9")
     var textBlock10 = $(".textarea10")
     var textBlock11 = $(".textarea11")
@@ -12,67 +13,61 @@ $(document).ready(function() {
     var textBlock15 = $(".textarea15")
     var textBlock16 = $(".textarea16")
     var textBlock17 = $(".textarea17")
-    var setHour9 = 9;
-    var setHour10 = 10;
-    var setHour11 = 11;
-    var setHour12 = 12;
-    var setHour13 = 13;
-    var setHour14 = 14;
-    var setHour15 = 15;
-    var setHour16 = 16;
-    var setHour17 = 17;
-
-    var presentHour = moment().format("kk");
-
+    
+    // Functions to change textblock colors
     function change9Color () {
-        if (setHour9 < presentHour) {
+        if (presentHour > 9) {
             textBlock9.addClass("past");
-        } else if (setHour9 == presentHour) {
+        } else if (presentHour == 9) {
             textBlock9.addClass("present")    
         } else {
             textBlock9.addClass("future");
         }
-    }    
+    }
+
     function change10Color () {
-        if (setHour10 < presentHour) {
+        if (presentHour > 10) {
             textBlock10.addClass("past");
-        } else if (setHour10 == presentHour) {
+        } else if (presentHour == 10) {
             textBlock10.addClass("present")    
         } else {
             textBlock10.addClass("future");
         }
     }
+
     function change11Color () {
-        if (setHour11 < presentHour) {
+        if (presentHour > 11) {
             textBlock11.addClass("past");
-        } else if (setHour11 == presentHour) {
+        } else if (presentHour == 11) {
             textBlock11.addClass("present")    
         } else {
             textBlock11.addClass("future");
         }
     }
+
     function change12Color () {
-        if (setHour12 < presentHour) {
+        if (presentHour) {
             textBlock12.addClass("past");
-        } else if (setHour12 == presentHour) {
+        } else if (presentHour == 12) {
             textBlock12.addClass("present")    
         } else {
             textBlock12.addClass("future");
         }
     }
+
     function change13Color () {
-        if (setHour13 < presentHour) {
+        if (presentHour > 13) {
             textBlock13.addClass("past");
-        } else if (setHour13 == presentHour) {
+        } else if (presentHour == 13) {
             textBlock13.addClass("present")    
         } else {
             textBlock13.addClass("future");
         }
     }
     function change14Color () {
-        if (setHour14 < presentHour) {
+        if (presentHour > 14) {
             textBlock14.addClass("past");
-        } else if (setHour14 == presentHour) {
+        } else if (presentHour == 14) {
             textBlock14.addClass("present")    
         } else {
             textBlock14.addClass("future");
@@ -80,32 +75,35 @@ $(document).ready(function() {
     }
 
     function change15Color () {
-        if (setHour15 < presentHour) {
+        if (presentHour > 15) {
             textBlock15.addClass("past");
-        } else if (setHour15 == presentHour) {
+        } else if (presentHour == 15) {
             textBlock15.addClass("present")    
         } else {
             textBlock15.addClass("future");
         }
     }
+
     function change16Color () {
-        if (setHour16 < presentHour) {
+        if (presentHour > 16) {
             textBlock16.addClass("past");
-        } else if (setHour16 == presentHour) {
+        } else if (presentHour == 16) {
             textBlock16.addClass("present")    
         } else {
             textBlock16.addClass("future");
         }
     }
+
     function change17Color () {
-        if (setHour17 < presentHour) {
+        if (presentHour > 17) {
             textBlock17.addClass("past");
-        } else if (setHour17 == presentHour) {
+        } else if (presentHour == 17) {
             textBlock17.addClass("present")    
         } else {
             textBlock17.addClass("future");
         }
     }
+
     change9Color();
     change10Color();
     change11Color();
@@ -116,7 +114,7 @@ $(document).ready(function() {
     change16Color();
     change17Color();
 
-    // Saves text in localStorage for that specific time
+    // Declare variables to store each textarea's value
     var text9 = $(".textarea9").val();
     var text10 = $(".textarea10").val();
     var text11 = $(".textarea11").val();
@@ -126,7 +124,8 @@ $(document).ready(function() {
     var text15 = $(".textarea15").val();
     var text16 = $(".textarea16").val();
     var text17 = $(".textarea17").val();
-
+    
+    // Saves text in localStorage for that specific time
     function init() {  
         $(".textarea9").val(localStorage.getItem("9am"));        
         $(".textarea10").val(localStorage.getItem("10am"));        
@@ -140,51 +139,52 @@ $(document).ready(function() {
         
         $(document).on("click", function(event) {
             if (event.target.matches(".saveBtn")){
-                var text9 = $(".textarea9").val();
+                text9 = $(".textarea9").val();
                 localStorage.setItem ("9am", text9);
             }
             
             if (event.target.matches(".saveBtn")){
-                var text10 = $(".textarea10").val();
+                text10 = $(".textarea10").val();
                 localStorage.setItem ("10am", text10);
             }
             
             if (event.target.matches(".saveBtn")){
-                var text11 = $(".textarea11").val();
+                text11 = $(".textarea11").val();
                 localStorage.setItem ("11am", text11);
             }
             
             if (event.target.matches(".saveBtn")){
-                var text12 = $(".textarea12").val();
+                text12 = $(".textarea12").val();
                 localStorage.setItem ("12pm", text12);
             }
             
             if (event.target.matches(".saveBtn")){
-                var text13 = $(".textarea13").val();
+                text13 = $(".textarea13").val();
                 localStorage.setItem ("13pm", text13);
             }
             
             if (event.target.matches(".saveBtn")){
-                var text14 = $(".textarea14").val();
+                text14 = $(".textarea14").val();
                 localStorage.setItem ("14pm", text14);
             }
             
             if (event.target.matches(".saveBtn")){
-                var text15 = $(".textarea15").val();
+                text15 = $(".textarea15").val();
                 localStorage.setItem ("15pm", text15);
             }
             
             if (event.target.matches(".saveBtn")){
-                var text16 = $(".textarea16").val();
+                text16 = $(".textarea16").val();
                 localStorage.setItem ("16pm", text16);
             }
             
             if (event.target.matches(".saveBtn")){
-                var text17 = $(".textarea17").val();
+                text17 = $(".textarea17").val();
                 localStorage.setItem ("17pm", text17);
             }           
             
         });
     }
+    
     init();
 });
